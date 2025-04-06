f2f9202624e4b455f478fcbf721843d1ea19bcb4
StealthWorm <thierrypitela@hotmail.com>
2025-04-06 11:51:41 -0300

    refactor: included resizing event to the plant images and drag styles to the cursor

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 /* The solution to draggable elements was inspired by w3schools solution on creating a [Draggable HTML Element](https://www.w3schools.com/howto/howto_js_draggable.asp). */
 
-dragElement(document.getElementById('plant1'), false);
-dragElement(document.getElementById('plant2'), false);
-dragElement(document.getElementById('plant3'), false);
-dragElement(document.getElementById('plant4'), false);
-dragElement(document.getElementById('plant5'), false);
-dragElement(document.getElementById('plant6'), false);
-dragElement(document.getElementById('plant7'), false);
-dragElement(document.getElementById('plant8'), false);
-dragElement(document.getElementById('plant9'), false);
-dragElement(document.getElementById('plant10'), false);
+dragElement(document.getElementById('plant1'));
+dragElement(document.getElementById('plant2'));
+dragElement(document.getElementById('plant3'));
+dragElement(document.getElementById('plant4'));
+dragElement(document.getElementById('plant5'));
+dragElement(document.getElementById('plant6'));
+dragElement(document.getElementById('plant7'));
+dragElement(document.getElementById('plant8'));
+dragElement(document.getElementById('plant9'));
+dragElement(document.getElementById('plant10'));
 
 /*"A closure is the combination of a function bundled together (enclosed) 
 with references to its surrounding state (the lexical environment). 
-In other words, a closure gives you access to an outer function’s scope 
+In other words, a closure gives you access to an outer function's scope 
 from an inner function." Create a closure so that you can track the dragged element*/
 
-function dragElement(terrariumElement, isClone) {
+function dragElement(terrariumElement) {
   // Set 4 positions for positioning on the screen
   let pos1 = 0,
     pos2 = 0,
@@ -42,15 +42,39 @@ function dragElement(terrariumElement, isClone) {
       clonedElement = terrariumElement;
     }
 
-    // Make the cloned element draggable
-
     // Get the initial mouse cursor position for pos3 and pos4
     pos3 = e.clientX;
     pos4 = e.clientY;
 
+    document.addEventListener('keyup', function (e) {
+      if (!e.shiftKey) {
+        clonedElement.style.backgroundColor = 'transparent';
+        clonedElement.style.cursor = document.onpointermove
+          ? 'move'
+          : 'default';
+      }
+    });
+
+    document.addEventListener('keydown', function (e) {
+      if (e.shiftKey) {
+        clonedElement.style.cursor = 'nw-resize';
+        clonedElement.style.backgroundColor = 'rgba(0, 0, 0, 0.36)';
+      }
+    });
+
     // When the mouse moves, start the drag
     document.onpointermove = function (e) {
-      elementDrag(e, clonedElement);
+      if (e.shiftKey) {
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        clonedElement.style.width = clonedElement.offsetWidth - pos1 + 'px';
+        clonedElement.style.height = clonedElement.offsetHeight - pos2 + 'px';
+      } else {
+        elementDrag(e, clonedElement);
+      }
     };
 
     // When the mouse is lifted, stop the drag
@@ -73,5 +97,6 @@ function dragElement(terrariumElement, isClone) {
     // Stop calculating when mouse is released
     document.onpointerup = null;
     document.onpointermove = null;
+    document.onkeydown = null;
   }
 }
